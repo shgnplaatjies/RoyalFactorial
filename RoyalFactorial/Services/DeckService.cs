51227acd7b52fb85299b57acea7de5d5941eeea4
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-15 01:46:24 +0200

    Adds DeckService Interface and Concrete DeckService
    - Adds deck service using an interface to implement Shuffle and Deal cards methods
    
    - Using an interface allows for abstraction of the implementation and extensibility.
    
    - Used somewhat 'unnecessary' variable names for legible code, as the performance impact of instantiation is negligble.
    
    - Applied modern .NET 8 patterns like LINQ for readible nested loops.
    
    - Adds error handling for exception, accounting for arbitrary player counts, deck size and count, and cards per player.
    
    - Decided to decouple shuffling the deck from the constructor to keep Single responsibility principle (might need a unshuffled deck for UI in future, who knows)

---
+++ RoyalFactorial/Services/DeckService.cs
@@ -0,0 +1,55 @@
+﻿using RoyalFactorial.MVVM.Model;
+using RoyalFactorial.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalFactorial.Services
+{
+    public class DeckService : IDeckService
+    {
+        private List<Card> _deck;
+        public int NumDecks { get; }
+
+        public DeckService(int numDecks)
+        {
+            List<Rank> availableRanks = Card.GetRanks();
+            List<Suit> availableSuits = Card.GetSuits();
+            this.NumDecks = numDecks;
+
+            _deck = (from rank in availableRanks
+                     from suit in availableSuits
+                     from deck in Enumerable.Range(0, numDecks)
+                     select new Card(suit, rank)).ToList();
+        }
+
+        public void ShuffleDeck() =>
+            _deck = new List<Card>(_deck.OrderBy(_ => Guid.NewGuid()));
+
+        public List<List<Card>> DealCards(int numPlayers, int numCardsPerPlayer)
+        {
+            var numCardsRequired = numPlayers * numCardsPerPlayer;
+
+            if (numCardsRequired > _deck.Count)
+                throw new InvalidOperationException(
+                    $"Not enough cards available for all players." +
+                    $"Required: {numCardsRequired}," +
+                    $"Available: {_deck.Count}");
+
+            var undealtCards = _deck.Skip(numCardsRequired).ToList();
+
+            _deck = undealtCards;
+
+            var players = Enumerable.Range(0, numPlayers);
+
+            return players
+                .Select(
+                    i => _deck.Skip(i * numCardsPerPlayer)
+                        .Take(numCardsPerPlayer)
+                        .ToList()
+                ).ToList();
+        }
+    }
+}
