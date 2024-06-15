8b0a588e4d2d850bd5f1713499a20a3d89ad3ad7
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-15 21:10:20 +0200

    Adds CardGame Service with Interface
    - Adds Interface to include Initialization function that takes in a List<string> of names instead of List<Player>.
      - This adds extends the card game's interface to work with different implementations of 'Player' while sticking to it's own internal Player definition.
      - Adds numberOfCardsPerPlayer and numberOfDecks to add flexibility beyond the provided spec of two decks and 6 players.
    
    - Initializes the Players and Leaderboard lazily by using the null forgiveness operator (null!) allowing for initialization with InitGame public method.
    
    - All processing for a game occurs within the initialization step of a CardGame and subsequent interactions are static, for a performant approach.

---
+++ RoyalFactorial/Services/CardGame.cs
@@ -0,0 +1,36 @@
+﻿using RoyalFactorial.MVVM.Model;
+using RoyalFactorial.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalFactorial.Services
+{
+    public class CardGame : ICardGame
+    {
+        public List<Player> Players { get; private set; } = null!;
+
+        public Leaderboard Leaderboard { get; private set; } = null!;
+
+        public void InitGame(List<string> playerNames, int numberOfCardsPerPlayer, int numberOfDecks)
+        {
+            var deckService = new DeckService(numberOfDecks);
+
+            deckService.ShuffleDeck();
+
+            var hands = deckService.DealCards(playerNames.Count, numberOfCardsPerPlayer);
+
+            Players = new(
+                playerNames
+                .Select(
+                    (name, index) => new Player(name, hands[index])
+                    )
+                );
+
+            Leaderboard = new(Players);
+        }
+    }
+}
---
+++ RoyalFactorial/Services/Interfaces/ICardGame.cs
@@ -0,0 +1,16 @@
+﻿using RoyalFactorial.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalFactorial.Services.Interfaces
+{
+    public interface ICardGame
+    {
+        List<Player> Players { get; }
+        Leaderboard Leaderboard { get; }
+        void InitGame(List<string> playerNames, int numberOfCardsPerPlayer, int numberOfDecks);
+    }
+}
