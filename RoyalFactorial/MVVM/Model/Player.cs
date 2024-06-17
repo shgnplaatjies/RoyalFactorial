4b0d8f0385c3008ced882d7093df5817815f179f
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-17 07:05:31 +0200

    GetSuitScore now includes a check for isTiedFirst to determine whether it will execute or not.
    - Small change to align closer to the spec, which requires Suit Scores to only be calculated for tied players vs only shown for tied players.
    
    - best case: O(1) when not tied.
    - worst case: O(n) when tied.

--- RoyalFactorial/MVVM/Model/Player.cs
+++ RoyalFactorial/MVVM/Model/Player.cs
@@ -9,7 +9,7 @@ namespace RoyalFactorial.MVVM.Model
     public class Player(string Name, List<Card> Hand)
     {
         public int RankScore { get; } = CalculateRankScore(Hand);
-        public int GetSuitScore => CalculateSuitScore(Hand);
+        public int GetSuitScore => CalculateSuitScore(Hand, IsTiedFirst);
         public string Name { get; } = Name;
         public int? Position { get; set; }
         public bool? IsTiedFirst { get; set; }
@@ -20,12 +20,15 @@ namespace RoyalFactorial.MVVM.Model
         private static int CalculateRankScore(List<Card> hand) =>
             hand.Sum(_ => _.RankScore);
 
-        private static int CalculateSuitScore(List<Card> hand) => hand
-            .Aggregate
+        private static int CalculateSuitScore(List<Card> hand, bool? isTiedFirst)
+        {
+            if (!(isTiedFirst ?? false)) return -1;
+
+            return hand.Aggregate
             (
                 1,
                 (product, card) => product * card.SuitScore
             );
-
+        }
     }
 }
