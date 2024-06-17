a08457aa4abaef8ee44186a053218668b5d89184
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-17 03:45:25 +0200

    Adds generic delegate/relay command.
    - Adds generic delegate to handle bound data as parameters in Commands.

--- RoyalFactorial/Core/Commands/DelegateCommand.cs
+++ RoyalFactorial/Core/Commands/DelegateCommand.cs
@@ -30,4 +30,51 @@ namespace RoyalFactorial.Core.Commands
 
         public void Execute(object? parameter) => _execute();
     }
+
+    public class DelegateCommand<T>(Action<T> execute, Func<T, bool>? canExecute = null) : ICommand
+    {
+        private readonly Action<T> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        private readonly Func<T, bool>? _canExecute = canExecute;
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add
+        {
+            if (_canExecute != null)
+                CommandManager.RequerySuggested += value;
+        }
+        remove
+        {
+            if (_canExecute != null)
+                CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (_canExecute == null) return true;
+        if (parameter == null && typeof(T).IsValueType) return _canExecute(default!);
+
+        return parameter == null || parameter is T t && _canExecute(t);
+    }
+
+    public void Execute(object? parameter)
+    {
+        if (parameter == null)
+        {
+            if (typeof(T).IsValueType)
+            {
+                _execute(default!);
+            }
+            else
+            {
+                throw new ArgumentException("Parameter is required.", nameof(parameter));
+            }
+        }
+        else
+        {
+            _execute((T)parameter);
+        }
+    }
+}
 }
